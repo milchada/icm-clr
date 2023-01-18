274fb21099c0b0fc17f7a9fcdc7535776e04a01b
Lukas Eisert <eisert@mpia.de>
2023-01-18 13:05:54 +0000

    Update run_params_opt.sh, scripts/model/params_opt.py

--- run_params_opt.sh
+++ run_params_opt.sh
@@ -8,10 +8,9 @@
 #SBATCH -J params_opt
 #
 #SBATCH --ntasks=1
-#SBATCH --constraint="gpu"
 #
 # --- default case: use a single GPU on a shared node ---
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:a100:4
 #SBATCH --cpus-per-task=18
 #SBATCH --mem=125000
 #
@@ -20,16 +19,9 @@
 #SBATCH --time=23:00:00
 
 module load anaconda/3
+module load cuda/11.4   
+module load cudnn/8.2.4
+module load pytorch/gpu-cuda-11.4/1.11.0  
+conda activate ergo
 
-module load intel/19.1.3
-module load impi/2019.9
-module load fftw-serial
-module load hdf5-serial
-module load gsl
-
-module load cuda/11.2
-module load cudnn/8.1.0
-module load pytorch/gpu-cuda-11.2/1.8.1
-conda activate simclr
-
-srun python -m scripts.util.params_opt
+srun python -m scripts.model.params_opt
